00e6045dfe399d2ff96319a252c2e377ebe2d8b7
Martin Terziisky <mterziisky@newbranch.tech>
2023-12-19 13:21:48 +0200

    Initial code setup.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Multi-Range selector</h1>
+
+      <div className="container">
+        <div className="left-handle"></div>
+        <div className="right-handle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
